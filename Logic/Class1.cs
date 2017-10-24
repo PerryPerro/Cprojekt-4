74c7273e6dcb7fb7de9386924c0568e0ddffc416
PerryPerro <Andreas.persson09@gmail.com>
2017-10-24 21:02:17 +0200

    Add files via upload

---
+++ Logic/Class1.cs
@@ -0,0 +1,229 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using System.Xml.XPath;
+using static Data.Pods;
+
+namespace Logic 
+{
+
+    
+    public class addPods
+    {
+        private Stream ms = new MemoryStream();
+       static List<Pod> allaPoddar = new List<Pod>();
+        
+        public addPods()
+        {
+            Stream stream = File.Open("PodData.dat", FileMode.Create);
+            BinaryFormatter bf = new BinaryFormatter();
+
+            bf.Serialize(stream, allaPoddar);
+            stream.Close();
+
+            stream = File.Open("PodData.dat", FileMode.Open);
+            bf = new BinaryFormatter();
+        }
+        
+        public static void addList(string url,string kategori, double intervall)
+        {
+            Pod varjePodcast = new Pod(url,kategori,intervall);
+            allaPoddar.Add(varjePodcast);
+        }
+        public static void serializePods()
+        {
+
+            using (Stream fs = new FileStream(@"C:\lista.xml", FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof
+                        (List<Pod>));
+                serializer.Serialize(fs, allaPoddar);
+
+            }
+          
+        }
+        public static void deSerializePods()
+        {
+            XmlSerializer serializer2 = new XmlSerializer(typeof(List<Pod>));
+
+            using (FileStream fs2 = File.OpenRead(@"C:\lista.xml"))
+            {
+                allaPoddar = (List<Pod>)serializer2.Deserialize(fs2);
+            }
+        }
+
+        public static void changeFeedUrl(string url, string newUrl, string newCategory, string newUpdateIntervall)
+        {
+            //var doc = XElement.Load(@"C:\lista.xml");
+            //var saveGame = doc
+            //     .Element("ArrayOfPod")
+            //     .Elements("Pod")
+            //     .Where(e => e.Element("Url").Value == "http://api.sr.se/api/rss/pod/3966")
+            //     .FirstOrDefault();
+
+            //saveGame.Element("UpdateIntervall").Value = "50";
+
+            //doc.Save(@"C:\lista.xml");
+
+            //var xdoc = XDocument.Load(@"C:\lista.xml");
+
+            ////xdoc.XPathSelectElement($"//Pod/Url[text()='{category}']/../UpdateIntervall").Value = "333333";
+            //xdoc.XPathSelectElements($"//ArrayOfPod/Pod").Remove();
+            //xdoc.Save(@"C:\lista.xml");
+
+            //var xDoc = XDocument.Load(@"C:\lista.xml");
+            //string xPath = "//ArrayOfPod/Pod/D";
+            ////or string xPath = "//B2/C3/D4[@id='1']";
+
+            //var eleList = xDoc.XPathSelectElements(xPath).ToList();
+            //foreach (var xElement in eleList)
+            //{
+            //    Debug.WriteLine(xElement);
+            //}
+
+            var xmlDoc = XDocument.Load(@"C:\lista.xml");
+            //XDocument xmlDoc = XDocument.Parse(@"C:\lista.xml");
+
+            var items = from item in xmlDoc.Descendants("Pod")
+                        where item.Element("Url").Value == url
+                        select item;
+
+            foreach (XElement itemElement in items)
+            {
+                itemElement.SetElementValue("Url", newUrl);
+                itemElement.SetElementValue("Category", newCategory);
+                itemElement.SetElementValue("UpdateIntervall", newUpdateIntervall);
+            }
+
+            xmlDoc.Save(@"C:\lista.xml");
+        }
+
+        public static void changeFeedCategory(string url, string newCategory)
+        {
+         
+            var xmlDoc = XDocument.Load(@"C:\lista.xml");
+            //XDocument xmlDoc = XDocument.Parse(@"C:\lista.xml");
+
+            var items = from item in xmlDoc.Descendants("Pod")
+                        where item.Element("Url").Value == url
+                        select item;
+
+            foreach (XElement itemElement in items)
+            {
+                itemElement.SetElementValue("Category", newCategory);
+            }
+
+            xmlDoc.Save(@"C:\lista.xml");
+        }
+
+        public static void changeFeedUpdateInteval(string url, string newInterval)
+        {
+
+            var xmlDoc = XDocument.Load(@"C:\lista.xml");
+            //XDocument xmlDoc = XDocument.Parse(@"C:\lista.xml");
+
+            var items = from item in xmlDoc.Descendants("Pod")
+                        where item.Element("Url").Value == url
+                        select item;
+
+            foreach (XElement itemElement in items)
+            {
+                itemElement.SetElementValue("UpdateIntervall", newInterval);
+            }
+
+            xmlDoc.Save(@"C:\lista.xml");
+        }
+
+        public static List<string> fyllComboBoxMedUrl()
+        {
+
+            var xmlDoc = XDocument.Load(@"C:\lista.xml");
+            //XDocument xmlDoc = XDocument.Parse(@"C:\lista.xml");
+
+            var items = from item in xmlDoc.Descendants("Pod")
+                        where item.Element("Url").Value != null
+                        select item.Element("Url").Value;
+            List<string> kategorier = new List<string>();
+            foreach (string itemElement in items)
+            {
+               kategorier.Add(itemElement);
+            }
+
+            return kategorier;
+        }
+
+        public static List<string> fyllComboBoxMedKategori()
+        {
+
+            var xmlDoc = XDocument.Load(@"C:\lista.xml");
+            //XDocument xmlDoc = XDocument.Parse(@"C:\lista.xml");
+
+            var items = from item in xmlDoc.Descendants("Pod")
+                        where item.Element("Category").Value != null
+                        select item.Element("Category").Value;
+            List<string> kategorier = new List<string>();
+            string hej = items.ToString();
+            foreach (string itemElement in items)
+            {
+                if (!kategorier.Contains(itemElement.ToString()))
+                {
+                    kategorier.Add(itemElement);
+                }
+                
+            }
+
+            return kategorier;
+        }
+
+        public static List<string> fyllListboxMedFeeds(string category)
+        {
+
+            var xmlDoc = XDocument.Load(@"C:\lista.xml");
+            //XDocument xmlDoc = XDocument.Parse(@"C:\lista.xml");
+
+            var items = from item in xmlDoc.Descendants("Pod")
+                        where item.Element("Category").Value == category
+                        select item.Element("Url").Value;
+            List<string> feeds = new List<string>();
+            foreach (string itemElement in items)
+            {
+               
+                    feeds.Add(itemElement);
+            }
+
+            return feeds;
+        }
+
+        public static void removeFeed(string url)
+        {
+            //var xmlDoc = XDocument.Load(@"C:\lista.xml");
+            ////XDocument xmlDoc = XDocument.Parse(@"C:\lista.xml");
+
+            //var items = from item in xmlDoc.Descendants("Pod")
+            //            where item.Element("Url").Value == url
+            //            select item;
+
+            //foreach (XElement itemElement in items)
+            //{
+            //    itemElement.Remove();
+            //}
+
+            //xmlDoc.Save(@"C:\lista.xml");
+            var xdoc = XDocument.Load(@"C:\lista.xml");
+
+            //xdoc.XPathSelectElement($"//Pod/Url[text()='{url}']").Remove();
+            xdoc.XPathSelectElements($"//ArrayOfPod/Pod").Where(x => (string)x.Element("Url") == url).Remove();
+            xdoc.Save(@"C:\lista.xml");
+        }
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,293 @@
+﻿using Logic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using WMPLib;
+
+
+namespace WindowsFormsApp1
+{
+    
+    public partial class Form1 : Form
+    {
+        public string xml = "";
+        private List<string> pods = new List<string>();
+        WMPLib.WindowsMediaPlayer Player;
+        System.Xml.XmlDocument dom = new System.Xml.XmlDocument();
+        private Stream ms = new MemoryStream();
+
+        //List<Pod> allaPoddar = new List<Pod>();
+
+    public Form1()
+
+        {
+
+            if (!File.Exists(@"C:\lista.xml"))
+            {
+                addPods.serializePods();
+            }
+
+            InitializeComponent();
+            addPods.deSerializePods();
+            
+            comboBox2.Items.AddRange(addPods.fyllComboBoxMedUrl().ToArray());
+            comboBox1.Items.AddRange(addPods.fyllComboBoxMedKategori().ToArray());
+           
+
+
+
+
+
+            //Stream stream = File.Open("PodData.dat", FileMode.Create);
+            //BinaryFormatter bf = new BinaryFormatter();
+
+            //bf.Serialize(stream, allaPoddar);
+            //stream.Close();
+
+            //stream = File.Open("PodData.dat", FileMode.Open);
+            //bf = new BinaryFormatter();
+        }
+
+        public Dictionary<String, String> categorys = new Dictionary<String, String>();
+        public List<KeyValuePair<String, String>> kategorier = new List<KeyValuePair<String, String>>();
+
+        public List<string> Pods { get => Pods1; set => Pods1 = value; }
+        public List<string> Pods1 { get => pods; set => pods = value; }
+        public List<string> Pods2 { get => pods; set => pods = value; }
+
+        private class Hejsan : addPods
+        {
+
+        }
+
+    private void button1_Click(object sender, EventArgs e)
+        {
+            //Pod varjePodcast = new Pod(textBox1.Text,textBox3.Text , int.Parse(textBox4.Text));
+         addPods.addList(textBox1.Text, textBox3.Text, int.Parse(textBox4.Text));
+            //allaPoddar.Add(varjePodcast);
+            
+            using (var client = new System.Net.WebClient())
+            {
+                client.Encoding = Encoding.UTF8;
+                xml = client.DownloadString(textBox1.Text); 
+            }
+            dom.LoadXml(xml);
+
+            foreach (System.Xml.XmlNode item
+               in dom.DocumentElement.SelectNodes("channel/item"))
+            {
+                //Skriv ut dess titel.
+                var title = item.SelectSingleNode("title");
+                Pods.Add(item.SelectSingleNode("title").InnerText);
+                listBox1.Text = title.InnerText;
+                Console.WriteLine(title.InnerText);
+            }
+            
+                if (listBox1.Items.ToString() != textBox3.Text)
+                {
+                    listBox1.Items.Add(textBox3.Text);
+                }
+
+            
+
+
+            setCategory(textBox3.Text);
+            comboBox1.Text = textBox3.Text;
+            addPods.serializePods();
+
+        }
+        private void visaLista()
+        {
+            var valtItem = comboBox1.SelectedItem.ToString();
+            listBox2.Items.Add(categorys.ContainsKey(valtItem));
+        }
+        //private void serializePods()
+        //{
+
+        //    using (Stream fs = new FileStream(@"C:\lista.xml", FileMode.Create, FileAccess.Write, FileShare.None))
+        //    {
+        //        XmlSerializer serializer = new XmlSerializer(typeof
+        //                (List<Pod>));
+        //        serializer.Serialize(fs, allaPoddar);
+
+        //    }
+        //    //XmlSerializer serializer2 = new XmlSerializer(typeof
+        //    //    ())
+        //}
+        //private void deSerializePods()
+        //{
+        //    XmlSerializer serializer2 = new XmlSerializer(typeof(List<Pod>));
+
+        //    using (FileStream fs2 = File.OpenRead(@"C:\lista.xml"))
+        //    {
+        //        allaPoddar = (List<Pod>)serializer2.Deserialize(fs2);
+        //    }
+        //}
+        public void setCategory(string category)
+        {
+            //category = textBox3.Text.ToString();
+            //var choosenPod = listBox1.SelectedItem.ToString();
+            //kategorier.Add(new KeyValuePair<String, String>(category, choosenPod));
+            ////varför inte katergorier.Key?
+            //var item = new KeyValuePair<String, String>(category, choosenPod);
+            //if (comboBox1.Items.Contains(item.Key) == false)
+            //{
+            //    comboBox1.Items.Add(item.Key);
+            //}
+
+
+        }
+        private void fillListWithObjects()
+        {
+            listBox2.Items.Clear();
+ 
+            foreach (var item in kategorier)
+            {
+                if (comboBox1.SelectedItem.ToString() == item.Key)
+                {
+                    listBox2.Items.Add(item.Value);
+                }
+                
+            }
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //string curItem = listBox1.SelectedItem.ToString();
+            //textBox3.Text = curItem;
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox1.Items.AddRange(addPods.fyllListboxMedFeeds(comboBox1.SelectedItem.ToString()).ToArray());
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            fillListWithObjects();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (var client = new System.Net.WebClient())
+            {
+                client.Encoding = Encoding.UTF8;
+                xml = client.DownloadString(textBox1.Text);
+            }
+
+            //Skapa en objektrepresentation.
+            var dom = new System.Xml.XmlDocument();
+            dom.LoadXml(xml);
+
+            //Iterera igenom elementet item.
+            foreach (System.Xml.XmlNode item
+               in dom.DocumentElement.SelectNodes("channel/item"))
+            {
+                if (item.SelectSingleNode("title").InnerText == listBox2.SelectedItem.ToString())
+                {
+                    MessageBox.Show(item.SelectSingleNode("description").InnerText);
+                }
+                //Skriv ut dess titel.
+                
+                
+                
+            }
+     
+        }
+        //private void createXmlFile()
+        //{
+        //    if (!File.Exists("lista.xml"))
+        //    {
+        //        using (Stream fs = new FileStream(@"C:\lista.xml", FileMode.Create, FileAccess.Write, FileShare.None))
+        //        {
+        //            XmlSerializer serializer = new XmlSerializer(typeof
+        //                    (List<Pod>));
+        //            serializer.Serialize(fs, allaPoddar);
+
+        //        }
+        //    }
+        //}
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var filePath = "";
+            dom.LoadXml(xml);
+            foreach (System.Xml.XmlNode item
+               in dom.DocumentElement.SelectNodes("channel/item"))
+            {
+                if (item.SelectSingleNode("title").InnerText == listBox2.SelectedItem.ToString())
+                {
+                    
+                    filePath = item.SelectSingleNode("enclosure/@url").InnerText;
+                   
+                   Process.Start("rundll32.exe", "shell32.dll, OpenAs_RunDLL " + filePath);
+                }
+
+            
+          
+        }
+           
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            
+            addPods.changeFeedUrl(comboBox2.SelectedItem.ToString(), textBox5.Text, textBox6.Text, textBox7.Text);
+            comboBox2.Items.Clear();
+            comboBox2.Items.AddRange(addPods.fyllComboBoxMedUrl().ToArray());
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            addPods.removeFeed(comboBox2.SelectedItem.ToString());
+        }
+    }
+    }
